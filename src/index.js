448ba8d5b1b27b244de7fa6a3bfdb26d662d0f05
Ankur Gupta <gupta.ankur255@gmail.com>
2019-07-23 17:27:09 +0530

    Add Changes

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ const Task=require('./models/task')
 const userRouter=require('./routers/user');
 const taskRouter=require('./routers/task');
 const bcrypt=require('bcryptjs')
+const cors=require('cors');
 const app=express()
 const port=process.env.PORT || 3000
 
 app.use(express.json())
+app.use(cors())
 app.use(userRouter)
 app.use(taskRouter)
 
