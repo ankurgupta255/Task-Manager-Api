c869ad016e875625be70f0f4edcef577b25c3387
Ankur Gupta <gupta.ankur255@gmail.com>
2019-07-26 20:15:01 +0530

    Added CHanges

--- src/index.js
+++ src/index.js
@@ -7,13 +7,17 @@ const bcrypt=require('bcryptjs')
 const cors=require('cors');
 const app=express()
 const port=process.env.PORT || 3000
+const url='https://github.com/ankurgupta255/Task-Manager-Api'
 
 app.use(express.json())
 app.use(cors())
+
+app.get('/',(req,res)=>{
+	res.send(`Welcome to the Task-Manager-API. Refer to the Documentation at ${url} to get to know better how this API works...`)
+})
+
 app.use(userRouter)
 app.use(taskRouter)
-
-
 
 app.listen(port,()=>{
 	console.log('Server is up on port ' + port)
